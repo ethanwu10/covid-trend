8c28a025b7270c46819d5470708ab3d4060d0981
Ethan Wu <ethanwu10@gmail.com>
2020-03-30 12:17:05 -0400

    Redo main layout
    Use grid for main layout and also fix issue with positioning of hidden
    <input> elements

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useCallback, useMemo } from 'react';
-import { Flex, Box, Card, Link } from 'rebass'
+import { Box, Card, Link } from 'rebass'
 import { Label, Input, Checkbox } from '@rebass/forms'
 
 import _ from 'lodash'
@@ -33,62 +33,59 @@ function App() {
     if (filter === '') return Object.keys(states)
     return statesFuse.search(filter).map(r => r.item.short)
   }, [filter])
-  console.log(selected)
 
   return (
-    <Flex height='100%' p={2} sx={{
-      flexDirection: 'column',
-      fontFamily: 'body'
+    <Box p={2} sx={{
+      height: ['auto', '100%'],
+      fontFamily: 'body',
+      display: 'grid',
+      grid: [
+        '"chart" 70vh "controls" 70vh "footer"',
+        '"chart controls" minmax(0, 1fr) "footer footer" auto / minmax(0, 1fr) 20rem'
+      ],
+      gridGap: [2, 2, 3]
     }}>
-      <Flex sx={{
-        minHeight: 0,
-        flex: '0 1 100%',
-        flexDirection: ['column', 'row']
+      <Box sx={{gridArea: 'chart'}}>
+        <TrendGraph states={Object.keys(selected).filter((k) => selected[k])} avgSz={avgSz} />
+      </Box>
+      <Card as='form' p={3} sx={{
+        gridArea: 'controls',
+        display: 'grid',
+        gridTemplateRows: 'auto minmax(0, 1fr) auto',
+        gridGap: 2
       }}>
-        <Box width={[1, 2/3]}>
-          <TrendGraph states={Object.keys(selected).filter((k) => selected[k])} avgSz={avgSz} />
+        <Box>
+          <Label>Search</Label>
+          <Input value={filter} onChange={(e) => setFilter(e.target.value)} />
         </Box>
-        <Card as='form' width={[1, 1/3]} height={['60vh', '100%']} p={3}>
-          <Flex sx={{
-            height: '100%',
-            flexDirection: 'column'
-          }}>
-            <Box mb={2}>
-              <Label>Search</Label>
-              <Input value={filter} onChange={(e) => setFilter(e.target.value)} />
-            </Box>
-            <Box sx={{
-              flexGrow: 1,
-              overflowY: 'auto'
+        <Box sx={{
+          overflowY: 'auto'
+        }}>
+          {shownStates.map(s => (
+            <Label key={s} sx={{
+              position: 'relative' // make sure hidden <input> elements are positioned correctly
             }}>
-              {shownStates.map(s => (
-                <Label key={s}>
-                  <Checkbox
-                    checked={selected[s]}
-                    onChange={e => setSelectedK(e.target.checked, s)}
-                  />
-                  {states[s]}
-                </Label>
-              ))}
-            </Box>
-            <Box mt={2}>
-              <Label>Rolling average size:</Label>
-              <Input
-                type='number'
-                value={avgSz}
-                onChange={e => setAvgSz(Number.parseInt(e.target.value))}
+              <Checkbox
+                checked={selected[s]}
+                onChange={e => setSelectedK(e.target.checked, s)}
               />
-            </Box>
-          </Flex>
-        </Card>
-      </Flex>
-      <Box sx={{
-        flex: '0 0 auto',
-        fontSize: 1
-      }} px={3} pt={2}>
+              {states[s]}
+            </Label>
+          ))}
+        </Box>
+        <Box>
+          <Label>Rolling average size:</Label>
+          <Input
+            type='number'
+            value={avgSz}
+            onChange={e => setAvgSz(Number.parseInt(e.target.value))}
+          />
+        </Box>
+      </Card>
+      <Box px={3} sx={{gridArea: 'footer'}}>
         Created by <Link href='https://ethanwu.dev/'>Ethan Wu</Link>, based on <Link href='https://aatishb.com/covidtrends'>a project</Link> by <Link href='https://aatishb.com'>Aatish Bhatia</Link>. Data from <Link href='https://covidtracking.com'>covidtracking.com</Link>. Source <Link href='https://github.com/ethanwu10/covid-trend'>on GitHub</Link>.
       </Box>
-    </Flex>
+    </Box>
   );}
 
 export default App;
